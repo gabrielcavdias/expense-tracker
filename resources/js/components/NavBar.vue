928bfdcae8bf9b4db6589f104914936c501250d5
helio <helio@pop-os.localdomain>
2023-04-24 17:58:21 -0300

    add: repsonsive nav

---
+++ resources/js/components/NavBar.vue
@@ -0,0 +1,43 @@
+<script>
+import { Link } from "@inertiajs/vue3";
+export default {
+    computed: {
+        path() {
+            return window.location.pathname.substring(1);
+        },
+    },
+    components: {
+        Link,
+    },
+};
+</script>
+<template>
+    <nav
+        class="fixed bottom-0 md:bottom-auto md:top-0 md:max-w-2xl w-full grid grid-cols-3 md:flex md:gap-6 md:mt-2 bg-gray-900 md:bg-transparent border-t md:border-t-0 border-opacity-5"
+    >
+        <Link
+            href="/"
+            :class="`${
+                path == '' ? 'text-amber-300' : 'text-gray-400'
+            } md:hover:text-amber-200 flex flex-col md:flex-row md:gap-3 items-center justify-center  py-5 px-3`"
+        >
+            <i class="fa fa-house"></i>
+            <span>Início</span>
+        </Link>
+        <Link
+            href="/categories"
+            :class="`${
+                path == 'categories' ? 'text-amber-300' : 'text-gray-400'
+            } md:hover:text-amber-200 flex flex-col md:flex-row md:gap-3 items-center justify-center  py-5 px-3`"
+        >
+            <i class="fa-solid fa-sign-hanging"></i>
+            Categorias
+        </Link>
+        <button
+            class="text-gray-400 md:hover:text-amber-200 flex flex-col md:flex-row md:gap-3 items-center justify-center py-5 px-3"
+        >
+            <i class="fa fa-user"></i>
+            Conta
+        </button>
+    </nav>
+</template>
--- resources/js/layout/Layout.vue
+++ resources/js/layout/Layout.vue
@@ -1,6 +1,7 @@
 <script>
 import { router, Link, Head } from "@inertiajs/vue3";
 import DateSelector from "../components/DateSelector.vue";
+import NavBar from "../components/NavBar.vue";
 
 export default {
     methods: {
@@ -11,23 +12,29 @@ export default {
             router.post("/config/date", { month: date.month });
         },
     },
-    computed: {
-        path() {
-            return window.location.pathname.substring(1);
-        },
-    },
     components: {
         Link,
         Head,
         DateSelector,
+        NavBar,
     },
 };
 </script>
 <template>
-    <header class="mt-5 flex justify-between items-start px-2 md:px-0 relative">
-        <Link href="/" class="text-amber-400 text-xl hover:text-amber-300">
-            <i class="fa-solid fa-house"></i>
-        </Link>
+    <header
+        class="mt-5 flex justify-between items-start px-2 md:px-0 relative md:pt-16"
+    >
+        <div>
+            <div
+                class="rounded-full overflow-hidden outline outline-1 outline-amber-400 md:absolute md:top-0 md:right-2"
+            >
+                <img
+                    src="https://www.github.com/gabrielcavdias.png"
+                    alt="Foto do usuário logado"
+                    class="w-14 md:w-12 h-full object-cover"
+                />
+            </div>
+        </div>
         <h1 class="text-gray-200 text-xl mb-4 flex gap-2">
             <DateSelector @changeDate="changeDate" />
         </h1>
@@ -35,36 +42,9 @@ export default {
             class="text-amber-400 text-xl hover:text-amber-300 transition-all duration-300"
             @click="logout"
         >
-            <i class="fa-solid fa-right-from-bracket"></i>
+            <i class="fa-solid fa-right-from-bracket md:absolute md:top-2"></i>
         </button>
     </header>
     <slot></slot>
-    <div
-        class="fixed bottom-0 w-full grid md:hidden grid-cols-3 bg-gray-900 border-t border-opacity-5"
-    >
-        <Link
-            href="/"
-            :class="`${
-                path == '' ? 'text-amber-300' : 'text-gray-400'
-            } flex flex-col items-center justify-center py-5 px-3`"
-        >
-            <i class="fa fa-house"></i>
-            <span>Início</span>
-        </Link>
-        <Link
-            href="/categories"
-            :class="`${
-                path == 'categories' ? 'text-amber-300' : 'text-gray-400'
-            } flex flex-col items-center justify-center py-5 px-3`"
-        >
-            <i class="fa-solid fa-sign-hanging"></i>
-            Categorias
-        </Link>
-        <button
-            class="text-gray-400 flex flex-col items-center justify-center py-5 px-3"
-        >
-            <i class="fa fa-user"></i>
-            Conta
-        </button>
-    </div>
+    <NavBar />
 </template>
